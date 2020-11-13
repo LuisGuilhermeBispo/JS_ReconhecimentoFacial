62d9c1a0de32567cb6862a43bd8a33168948d85a
Luis Guilherme Bezerra Bispo <luguilherm@engdb.infra>
2020-11-13 00:41:23 -0300

    Reconhecimento de Persona

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,7 +10,7 @@ const startVideo = () => {
                     if (device.kind === 'videoinput') {
                         //é uma camera
                         console.log(device)
-                        if (device.label.includes('')) {
+                        if (device.label.includes('Integrated Webcam')) {
                             navigator.getUserMedia({
                                     video: {
                                         deviceId: device.deviceId
@@ -24,6 +24,22 @@ const startVideo = () => {
                 })
             }
         })
+}
+
+const loadLabels = () => {
+    const labels = ['Luis Bispo']
+    return Promise.all(labels.map(async label => {
+        const descriptions = []
+        for(let i = 1; i <= 4; i++){
+            const img = await faceapi.fetchImage(`/assets/lib/face-api/labels/${label}/${i}.JPG`)
+            const detections = await faceapi
+                .detectSingleFace(img)
+                .withFaceLandmarks()
+                .withFaceDescriptor()
+                descriptions.push(detections.descriptor)
+        }
+        return new faceapi.LabeledFaceDescriptors(label, descriptions)
+    }))
 }
 
 Promise.all([
@@ -41,12 +57,41 @@ cam.addEventListener('play', async () => {
         width: cam.width,
         height: cam.height
     }
+    const labels = await loadLabels()
     faceapi.matchDimensions(canvas, canvasSize)
     document.body.appendChild(canvas)
     setInterval(async () => {
-       const detections = await faceapi.detectAllFaces(cam, new faceapi.TinyFaceDetectorOptions())
+       const detections = await faceapi.
+       detectAllFaces(
+           cam, 
+           new faceapi.TinyFaceDetectorOptions()
+           )
+           .withFaceLandmarks()
+           .withFaceExpressions()
+           .withAgeAndGender()
+           .withFaceDescriptors()
        const resizedDetections = faceapi.resizeResults(detections, canvasSize)
-       canvas.getContext('2d').clearRect(0,0, canvas.width, canvas.height)
+       const faceMatcher = new faceapi.FaceMatcher(labels, 0.6)
+       const results = resizedDetections.map(d =>
+        faceMatcher.findBestMatch(d.descriptor)
+    )
+       canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
        faceapi.draw.drawDetections(canvas, resizedDetections)
-    }, 100)
+       faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+       faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+       resizedDetections.forEach( detection => {
+           const { age, gender, genderProbability } = detection
+           new faceapi.draw.DrawTextField([
+                   `${parseInt(age, 10)} years`,
+                   `${gender} (${parseInt(genderProbability * 100, 10)})`
+               ], detection.detection.box.topRight).draw(canvas)
+       })
+       results.forEach((result, index) => {
+        const box = resizedDetections[index].detection.box
+        const { label, distance } = result
+        new faceapi.draw.DrawTextField([
+            `${label} (${parseInt(distance * 100, 10)})`
+        ], box.bottomRight).draw(canvas)
+    })
+}, 100)
 })
